24f24672c7f2c0770c2d038fe35a70cebe021a0b
Garrett Granroth <granrothge@users.noreply.github.com>
2009-06-10 20:34:43 +0000

    added a routine to read info from runinfo file

---
+++ dgs/trunk/python/log_proc.py
@@ -0,0 +1,55 @@
+from xml.dom.minidom import *
+def runinfo(instrument,proposal,runnum):
+     # possible instrument names
+     posinst=set(['ARCS','SEQ','CNCS','BASIS'])
+     if not set([instrument]).issubset(posinst):
+          raise ValueError, "must be a short instrument name"
+     instrun='%s_%i' %(instrument,runnum)
+     filestr=r'/'+instrument+r'-DAS-FS/'+proposal+r'/'+instrun+r'/'+instrun+'_runinfo.xml'
+     return read_info(filestr)
+def read_info(filename):
+      vainfo={}
+      dat=parse(filename)
+      akeys=dat.documentElement.attributes.keys()
+      for aitem in akeys:
+          vainfo[aitem]=dat.documentElement.attributes[aitem].value
+       
+      # get time and date
+      di=dat.childNodes[0].childNodes[1]
+      for idx in range(len(di.childNodes)):
+           locName=di.childNodes[idx].localName
+           if locName=='StartTime':
+	        evalstr="vainfo['"+locName+"']= '%s'" %(di.childNodes[idx].childNodes[0].nodeValue)
+		exec(evalstr)
+	   if locName=='StopTime':
+	        evalstr="vainfo['"+locName+"']= '%s'" %(di.childNodes[idx].childNodes[0].nodeValue)
+		exec(evalstr)
+      #operational info
+      oi=dat.childNodes[0].childNodes[3]
+      for idx in range(len(oi.childNodes)):
+          locName=oi.childNodes[idx].localName
+      	  if locName=='PCurrent':
+	  	evalstr="vainfo['"+locName+"']= %s" %(oi.childNodes[idx].childNodes[0].nodeValue)
+		exec(evalstr)
+      #Sample info
+      #dat.childNodes[0].childNodes[5]
+      #General info
+      gi=dat.childNodes[0].childNodes[7]
+      for idx in range(len(gi.childNodes)):
+          locName=gi.childNodes[idx].localName
+          if locName=='Title':
+	       evalstr="vainfo['"+locName+"']= '%s'" %(gi.childNodes[idx].childNodes[0].nodeValue)
+	       exec(evalstr)
+	  if locName=='Notes':
+	       evalstr="vainfo['"+locName+"']= '%s'" %(gi.childNodes[idx].childNodes[0].nodeValue)
+	       exec(evalstr)
+	  
+	       
+      # Detector info
+      dei=dat.childNodes[0].getElementsByTagName(u'DetectorInfo')[0].getElementsByTagName('Scattering')[0]
+      timechannel={}
+      akeys=dei.getElementsByTagName('NumTimeChannels')[0].attributes.keys()
+      for aitem in akeys:
+          timechannel[aitem]=dei.getElementsByTagName('NumTimeChannels')[0].attributes[aitem].value
+      vainfo['timing']=timechannel	       
+      return vainfo
