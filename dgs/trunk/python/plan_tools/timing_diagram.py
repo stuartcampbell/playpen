95a6679b5c69753cca7fe68ba7d7a9f837f53ddb
Garrett Granroth <granrothge@users.noreply.github.com>
2009-06-19 02:58:09 +0000

    updated python routines

--- dgs/trunk/python/plan_tools/timing_diagram.py
+++ dgs/trunk/python/plan_tools/timing_diagram.py
@@ -18,10 +18,16 @@ from pylab import figure, plot, show, hold, xlabel, ylabel, title, text, pi
 from matplotlib import rc
 
 def E2V(E):
+    """
+      Takes a neutron energy in meV and converts it to velocity in m/s 
+    """
 # for energy in mev returns velocity in m/s
     return sqrt(E/5.227e-6)
 
 def V2E(V):
+    """
+      Takes a neutron velocity in m/s and converts it to energy in meV
+    """
 # for  v in  m/s returns energy in meV
     return 5.227e-6*V*V
     
@@ -54,6 +60,10 @@ class Slit_pack():
     
     
 def timing_diagram(E,Ferminu,T0nu,spec,slit_pack,show_all=0):
+    """
+      routine to plot timing diagram to check for fram overlap
+      timing_diagram(E,Ferminu,T0nu,spec,slit_pack,show_all=0)
+    """
     # define possible spectrometers
     pos_spec=set(['ARCS','SEQUOIA'])
     tmpset=set([spec])
@@ -65,7 +75,7 @@ def timing_diagram(E,Ferminu,T0nu,spec,slit_pack,show_all=0):
         L=array([8.77,11.6,13.6,3.4])
     else:
         L=array([9.78,18.0,20.0,6.3])
-    dtT0=0.06/(0.2*T0nu*2.0*pi)/2.0
+    dtT0=0.10/(0.2*T0nu*2.0*pi)/2.0
     vcen=E2V(E)
     tcen=L[0]/vcen
     tT0min=tcen-dtT0/2.0
@@ -142,6 +152,56 @@ def timing_diagram(E,Ferminu,T0nu,spec,slit_pack,show_all=0):
     title(r'E=%g meV $\nu_{fermi}$=%g Hz $\nu_{T_0}$=%g Hz slit pack:%s'%(E,Ferminu,T0nu,slit_pack.name))
     show()
     
+def T0_only_timing(E,T0nu,spec):
+    """
+    plot timing diagram to check for frame overlap when no Fermi chopper is in the beam
+    """ 
+    # define possible spectrometers
+    pos_spec=set(['ARCS','SEQUOIA'])
+    tmpset=set([spec])
+    if not tmpset.issubset(pos_spec):
+        #raise RuntimeError, 'spectrometer must be'+pos_spec
+        raise RuntimeError,'spectrometer not defined'
+    #define lengths for spectrometers
+    if spec=='ARCS':
+        L=array([8.77,11.6,13.6,3.4])
+    else:
+        L=array([9.78,18.0,20.0,6.3])
+    dtT0=0.06/(0.2*T0nu*2.0*pi)/2.0
+    vcen=E2V(E)
+    tcen=L[0]/vcen
+    tT0min=tcen-dtT0/2.0
+    tT0max=tcen+dtT0/2.0
+    tcenf=L[1]/vcen
+    #number of T0 repetitions  per source pulse
+    npulse=int(T0nu/30.0);
+    # determine the maximum and minimum velocity for eacy pulse through the T0 chopper
+    vmax=zeros(npulse);
+    vmin=zeros(npulse);
+    tT0mins=zeros(npulse);
+    tT0maxs=zeros(npulse);
+    for idx in range(npulse):
+        tT0mins[idx]=(tT0min+idx*1.0/(2.0*T0nu))
+        tT0maxs[idx]=(tT0max+idx*1.0/(2.0*T0nu))
+    vmax=L[0]/tT0mins
+    vmin=L[0]/tT0maxs 
+    tmax=(L[2]+L[3])/vmin
+    tmin=(L[2]+L[3])/vmax
+    t0T0=zeros(tmax.shape)
+    tT0mins=concatenate((tT0mins,tT0mins+1.0/60.0))
+    tT0maxs=concatenate((tT0maxs,tT0maxs+1.0/60.0))
+    tmax=concatenate((tmax,tmax+1.0/60.0))
+    tmin=concatenate((tmin,tmin+1.0/60.0))
+    t0T0=concatenate((t0T0,t0T0+1.0/60.0))
+    figure()
+    hold(True)
+    for idx in range(len(tmax)):
+        plot([t0T0[idx],tmin[idx]],[0.0,L[2]+L[3]],'k')    
+        plot([t0T0[idx],tmax[idx]],[0.0,L[2]+L[3]],'k')
+    xlabel('t(s)')
+    ylabel('L(m)')
+    show()
+       
 #define defualt slit packages
 SEQ_100=Slit_pack(0.00203,0.58,'SEQ 100')
 SEQ_700=Slit_pack(0.00356,1.53,'SEQ 700')
