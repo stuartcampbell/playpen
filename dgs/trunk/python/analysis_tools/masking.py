7ce4dddab8fb44642f47a16bcede4c4774762236
Garrett Granroth <granrothge@users.noreply.github.com>
2009-11-18 20:17:44 +0000

    recent updates

--- dgs/trunk/python/analysis_tools/masking.py
+++ dgs/trunk/python/analysis_tools/masking.py
@@ -1,3 +1,5 @@
+
+
 def mask_file_write(filename,lst):
    fid=open(filename,'w')
    for idx in range(len(lst)):
@@ -8,6 +10,10 @@ def mask_create(bank):
     pixels=range(128)
     tubes=range(8)
     lst=[]
-    for idx in tubes:
-       lst.append(map(lambda x: "bank_%d_%d_%d\n"%(bank,idx,x),pixels))
+    for idx2 in range(len(bank)):
+      for idx in tubes:
+         lst.append(map(lambda x: "bank%d_%d_%d\n"%(bank[idx2],idx,x),pixels))
     return lst    
+
+
+    
