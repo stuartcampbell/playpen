79d1920c12b85aa4ae626c9f3245c5c48f9a3d1c
j35 <j35@dc6e9669-1f21-4d71-ab25-b014eae27bc5>
2010-02-03 02:29:57 +0000

    Modified program. This resolves #13

--- wc_idl_pl/wc_idl.pl
+++ wc_idl_pl/wc_idl.pl
@@ -14,36 +14,74 @@
 #
 # author: j35@ornl.gov
 
+
 #Name of file that contain all the files to take into account
 $config_file = "wc_config.txt"; 
 $output_file = "wc_output.txt";
 
-open(FILE,$config_file) or die "Can not open $config_file!\n";
-chomp(@list_of_files = (<FILE>)); #remove '\n' at the end of each line
-close FILE;
 
-open(OUTPUT_FILE,">$output_file") or die "can't create $output_file!\n";
 
-for $file (@list_of_files) {
-  if ($file =~ /^#{3,}/) {
-    print OUTPUT_FILE $file . "\n"; #name of folder
-} else { #name of file
-  print OUTPUT_FILE $file;
-  $code_ln_nbr = getNbrLines($file);
-  if ($code_ln_nbr == -1) {
-    print OUTPUT_FILE "$file (ERROR!)\n";
-  } else {
-    print OUTPUT_FILE "$file ($code_ln_nbr)\n";
-}
+@input_arg = @ARGV;
+if ($#input_arg == -1) {
+  display_manual();
+  exit;
 }
 
+if ($input_arg[0] eq '-c' || $input_arg[0] eq '--config') {
+  create_config_file();
+}
+elsif ($input_arg[0] eq '-h' || $input_arg[0] eq '--help') {
+  display_manual();
+} else {
+  count_line();
 }
 
-close OUTPUT_FILE;
+
+sub create_config_file {
+
+}
+
+
+
+
+sub   display_manual {
+  print "\nwc_idl [list_of_folders_to_check>/-c/-h]\n\n";
+  print "      -c, --config    will cretae the wc_config.txt file used by\n";
+  print "      -h, --help      will display this message";
+  print "wc_idl\n\n";
+}
+
+
+
+sub count_line {
+  
+  open(FILE,$config_file) or die "Can not open $config_file!\n";
+  chomp(@list_of_files = (<FILE>)); #remove '\n' at the end of each line
+  close FILE;
+  
+  open(OUTPUT_FILE,">$output_file") or die "can't create $output_file!\n";
+  
+  for $file (@list_of_files) {
+    if ($file =~ /^#{3,}/) {
+      print OUTPUT_FILE $file . "\n"; #name of folder
+    } else { #name of file
+      print OUTPUT_FILE $file;
+      $code_ln_nbr = getNbrLines($file);
+      if ($code_ln_nbr == -1) {
+	print OUTPUT_FILE "$file (ERROR!)\n";
+      } else {
+	print OUTPUT_FILE "$file ($code_ln_nbr)\n";
+      }
+    }
+  }
+  close OUTPUT_FILE;
+}
+
+
+
 
 # This subroutines count the number of lines the file contain
-sub getNbrLines {
-
+sub getNbrLines {  
   my (@file_array) = @_;
   my $file = $file_array[0];
   open(MY_FILE,$file) or return -1;
@@ -51,7 +89,6 @@ sub getNbrLines {
   $nbr_lines = $#line_array+1;
   close MY_FILE;
   return $nbr_lines;
-
 }
 
 
